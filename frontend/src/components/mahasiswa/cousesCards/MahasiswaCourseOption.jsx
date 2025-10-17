6d4a4cbbcbd6b3643ad2edf6f2c08bddb4d72291
heekal <haikal.aleee@gmail.com>
2025-10-17 16:33:18 +0700

    sedikit penyesuaian lah lupa gw:

--- frontend/src/components/mahasiswa/cousesCards/MahasiswaCourseOption.jsx
+++ frontend/src/components/mahasiswa/cousesCards/MahasiswaCourseOption.jsx
@@ -7,7 +7,7 @@ export default function MahasiswaCourseOption({course_name, path}) {
       to={path}
       className="
         gap-1 flex w-fit flex-row text-lg font-semibold text-stone-600 
-        hover:text-blue-600 hover:underline items-center cursor-pointer">
+        hover:text-stone-800 hover:underline items-center cursor-pointer">
       <FiArrowUpLeft size={20}/>
       <span>{course_name}</span>
     </NavLink>
