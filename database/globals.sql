e459d2a3e94144b6c4f19b152af208e1c69c13b5
heekal <haikal.aleee@gmail.com>
2025-11-22 10:03:49 +0700

    databse aman

---
+++ database/globals.sql
@@ -0,0 +1,35 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+\restrict chCrxDU29unVaeFINBAlllay7WyebNitQx7KbrBBJgiU8d7Sc4kUbHXpf9JqIth
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE postgres;
+ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:heJYCRdkpSiV5vS+FRGo7w==$7xsF7XDZFoXZ0pc92NGiPC4ZAD7Z7e3scGodnEO6dk0=:tJkPRIJS+psgDVdjtmvWdAlusovTgZf3hBugBcGWLiU=';
+
+--
+-- User Configurations
+--
+
+
+
+
+
+
+
+
+\unrestrict chCrxDU29unVaeFINBAlllay7WyebNitQx7KbrBBJgiU8d7Sc4kUbHXpf9JqIth
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
